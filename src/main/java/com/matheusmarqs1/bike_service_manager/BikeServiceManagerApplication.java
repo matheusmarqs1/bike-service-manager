78920e2143d158e6033a2e7e5140764f18e91cbf
matheusmarqs1 <matheusti770@gmail.com>
2026-03-05 15:21:43 -0300

    Project created

---
+++ src/main/java/com/matheusmarqs1/bike_service_manager/BikeServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmarqs1.bike_service_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeServiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeServiceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusmarqs1/bike_service_manager/BikeServiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmarqs1.bike_service_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeServiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
